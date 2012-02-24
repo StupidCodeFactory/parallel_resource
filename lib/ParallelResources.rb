c54744da91dc69fe570c7a89cb3c7463fc343349
Yann Marquet <yann.marquet@free.fr>
2012-02-24 13:47:29 +0100

    Update lib/ParallelResources.rb

--- lib/ParallelResources.rb
+++ lib/ParallelResources.rb
@@ -1,6 +1,6 @@
 $:.unshift(File.dirname(__FILE__)) unless
   $:.include?(File.dirname(__FILE__)) || $:.include?(File.expand_path(File.dirname(__FILE__)))
-
+# WhyTheHellThisFileCamelCased ????? damn_it!!!!
 module ParallelResources
   VERSION = '0.0.1'
   require 'rubygems'
